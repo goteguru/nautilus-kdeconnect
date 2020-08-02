f68c773c303caafb2df11f3d5e9852fb28186b54
Mészáros Gergely <monk@kormi.home.geotronic.hu>
2020-08-02 16:29:40 +0200

    fix:urlparse renamed to urllib.parse in python3 which is mostly default today

--- kdeconnect-ext.py
+++ kdeconnect-ext.py
@@ -4,7 +4,10 @@ from gi.repository import GObject
 from kdeconnect import send_files, get_available_devices
 import zipfile
 import urllib
-import urlparse
+try:
+    import urlparse
+except ModuleNotFoundError:
+    import urllib.parse
 
 TARGET = "%%TARGET%%".title()
 Nautilus = importlib.import_module("gi.repository.{}".format(TARGET))
